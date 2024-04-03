937bc5f12456f3328a4cb8f8f9f0dccac2c85207
Shakil_xBCC <shakilmrf8@gmail.com>
2024-04-03 23:08:57 +0600

    Added order History retrieval

--- src/db/routers/adminRouter.js
+++ src/db/routers/adminRouter.js
@@ -9,99 +9,116 @@ const router = express.Router();
 
 //****************************************************** GET REQUEST ********************************************************************* */
 
-/**
- * Renders the login-admin page.
- */
-router.get('/admin/login', async(req,res) =>{
-    try{
-        res.render('adminLogin');
-    }
-    catch(e){
-        res.status(500).send(e);
-    }
-});
-
-/**
- * Renders the main-admin page.
- */
-router.get('/admin/page', async (req, res) => {
-    try {
-        res.render('admin');
-    }
-    catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-/**
- * Verifies admin token and sends back the ID.
- */
- router.get('/admin-verify-token', adminAuth, async (req, res) => {
-    try {
-        const admin = await Admin.findOne({ ID: req.admin.ID });
-        if (!admin)
-            return res.status(404).send({ Message: 'Invalid credentials.' });
-        res.send( { admin } );
-    }
-    catch (e) {
-        res.status(500).send(e);
-    }
-});
-
-/**
- * Logout admin user by removing the token from the tokens array.
- */
- router.get('/admin/logout', async(req,res)=>{
-    const {token} = req.headers;
-    try{
-        const admin = await Admin.findOne({token});
-        for(let i = 0; i < admin.tokens.length; i++){
-            if( token === admin.tokens[i].token ){
-                admin.tokens[i].token += 'Im gonna ruin this token';
-                break;
-            }
+    /**
+     * Renders the login-admin page.
+     */
+    router.get('/admin/login', async(req,res) =>{
+        try{
+            res.render('adminLogin');
         }
-        await admin.save();
-        res.send();
-    } 
-    catch(e){
-        res.status(500).send(e);
-    }
-});
+        catch(e){
+            res.status(500).send(e);
+        }
+    });
+
+    /**
+     * Renders the main-admin page.
+     */
+    router.get('/admin/page', async (req, res) => {
+        try {
+            res.render('admin');
+        }
+        catch (e) {
+            res.status(500).send(e);
+        }
+    });
+
+    /**
+     * Verifies admin token and sends back the ID.
+     */
+    router.get('/admin-verify-token', adminAuth, async (req, res) => {
+        try {
+            const admin = await Admin.findOne({ ID: req.admin.ID });
+            if (!admin)
+                return res.status(404).send({ Message: 'Invalid credentials.' });
+            res.send( { admin } );
+        }
+        catch (e) {
+            res.status(500).send(e);
+        }
+    });
+
+    /**
+     * Logout admin user by removing the token from the tokens array.
+     */
+    router.get('/admin/logout', async(req,res)=>{
+        const {token} = req.headers;
+        try{
+            const admin = await Admin.findOne({token});
+            for(let i = 0; i < admin.tokens.length; i++){
+                if( token === admin.tokens[i].token ){
+                    admin.tokens[i].token += 'Im gonna ruin this token';
+                    break;
+                }
+            }
+            await admin.save();
+            res.send();
+        } 
+        catch(e){
+            res.status(500).send(e);
+        }
+    });
 
 
-//****************************************************** POST REQUEST ******************************************************************** */
+    //****************************************************** POST REQUEST ******************************************************************** */
+
+    /**
+     * Admin log in request. Checks if credentials are correct and grants a token.
+     */
+    router.post('/admin/login', async (req, res) => {
+        const loginInfo = req.body;
+        try {
+            const admin = await Admin.findOne({ ID: loginInfo.ID });
+            if (!admin)
+                return res.status(404).send({ Message: 'Invalid credentials.' });
+            const isMatch = await bcrypt.compare(loginInfo.password, admin.password);
+            if (!isMatch)
+                return res.status(404).send({ Message: 'Invalid credentials.' });
+            const token = await admin.generateToken();
+            res.send({ token });
+        }
+        catch (e) {
+            res.status(500).send(e);
+        }
+    });
+
+    module.exports = router;
+
 
 /**
- * Admin log in request. Checks if credentials are correct and grants a token.
+ * Retrieves the order history list.
  */
-router.post('/admin/login', async (req, res) => {
-    const loginInfo = req.body;
+router.get('/admin/orders', adminAuth, async (req, res) => {
     try {
-        const admin = await Admin.findOne({ ID: loginInfo.ID });
-        if (!admin)
-            return res.status(404).send({ Message: 'Invalid credentials.' });
-        const isMatch = await bcrypt.compare(loginInfo.password, admin.password);
-        if (!isMatch)
-            return res.status(404).send({ Message: 'Invalid credentials.' });
-        const token = await admin.generateToken();
-        res.send({ token });
-    }
-    catch (e) {
+        // Retrieve the order history from the database
+        const orders = await Order.find({ adminId: req.admin.ID }).populate('bookId').populate('userId');
+
+        if (!orders || orders.length === 0) {
+            return res.status(404).send({ Message: 'No orders found.' });
+        }
+
+        res.send({ orders });
+    } catch (e) {
         res.status(500).send(e);
     }
 });
 
-module.exports = router;
 
-
-
-
-// //////////////////////////////////////   Creates admin manually  /////////////////////////////////////////
-//                                                          //////////////////////////////////////////////
-// router.post('/create-admin', async(req,res)=>{              //////////////////////////////////////////////
-//     const admin = new Admin(req.body);                      //////////////////////////////////////////////
-//     admin.save();                                           //////////////////////////////////////////////
-//     res.send();                                             //////////////////////////////////////////////
-// });                                                   //////////////////////////////////////////////
-// /////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // //////////////////////////////////////   Creates admin manually  /////////////////////////////////////////
+    //                                                          //////////////////////////////////////////////
+    // router.post('/create-admin', async(req,res)=>{              //////////////////////////////////////////////
+    //     const admin = new Admin(req.body);                      //////////////////////////////////////////////
+    //     admin.save();                                           //////////////////////////////////////////////
+    //     res.send();                                             //////////////////////////////////////////////
+    // });                                                   //////////////////////////////////////////////
+    // /////////////////////////////////////////////////////////////////////////////////////////////////////////
